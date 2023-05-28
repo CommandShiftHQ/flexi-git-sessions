4182694bdebfca14df793e26902f2cae62e0cc95
StuartCowley <voidstalker@live.co.uk>
2023-05-28 15:38:28 +0100

    Added number methods & string methods

---
+++ string-methods.js
@@ -0,0 +1,15 @@
+const sayHello = (string) => {
+  return `Hello, ${string}!`;
+};
+
+const uppercase = (string) => {
+  return string.toUpperCase();
+};
+
+const lowercase = (string) => {
+  return string.toLowerCase();
+};
+
+const countCharacters = (string) => {
+  return string.length;
+};
